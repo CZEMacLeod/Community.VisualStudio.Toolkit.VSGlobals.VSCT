3d2c84448d7776e5f43e7de8970b43206437e2e7
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2021-04-10 17:11:12 +0100

    Add project files.

---
+++ VSIXProject1/Commands/MyCommand.cs
@@ -0,0 +1,20 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXProject1
+{
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        public MyCommand()
+            : base(PackageGuids.CommandSet, PackageIds.MyCommand) { }
+
+        protected override Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            VS.Notifications.ShowMessage("MyCommand", "Button clicked");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ VSIXProject1/VSIXProject1Package.cs
@@ -0,0 +1,22 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXProject1
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidPackageString)]
+    public sealed class VSIXProject1Package : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await MyCommand.InitializeAsync(this);
+        }
+    }
+}
